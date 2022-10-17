be5c58df35af805e371ef50592b35447f05a843f
Chris Crump <chriscrawford893@gmail.com>
2022-10-17 15:02:00 -0400

    Set TARGET_PREBUILT_KERNEL
    Change-Id: I1ab511c33188949b678397574589012585f0f7b0

--- device-aospa.mk
+++ device-aospa.mk
@@ -13,3 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+# Kernel
+TARGET_PREBUILT_KERNEL := device/google/pantah-kernel/Image.lz4
